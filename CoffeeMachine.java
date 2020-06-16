489be5680fbfd41647309e4ae02c9e715bf489da
victorhgo <39852859+victorhgo@users.noreply.github.com>
2020-06-16 20:59:45 -0300

    Update CoffeeMachine.java
    Created new method named startMachine that prints on screen information when we turn on the machine.

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -2,5 +2,19 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-
+    final static Scanner scanner = new Scanner(System.in);
+  
+    public static void main(String[] args) {
+        startMachine();
+    }
+  
+    static void startMachine() {    
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+    }  
 }
