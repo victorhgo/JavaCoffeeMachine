0d8122e2e7a5215efbd0db303e38287be0b1c17e
Victor Hugo <39852859+victorhgo@users.noreply.github.com>
2020-06-20 12:57:21 -0300

    Add request supply method
    The request supply method will analyze how much ingredients the machine has, and then it will calculate how much coffee it can produces, and it will inform if the quantity of ingredients is enough.

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,21 +1,24 @@
 /********************************************************
-* Created by Victor Hugo on 16th June, 2020.
-* Learning Java with JetBrains Academy Java Course.
-* A Simple Coffee Machine Written in Java.
-********************************************************/
+ * Created by Victor Hugo on 16th June, 2020.
+ * Learning Java with JetBrains Academy Java Course.
+ * A Simple Coffee Machine Written in Java.
+ ********************************************************/
 
 package machine;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CoffeeMachine {
     final static Scanner scanner = new Scanner(System.in);
-  
+
     public static void main(String[] args) {
-        startMachine();
-        howManyCoffee();
+        //startMachine();
+        //howManyCoffee();
+        requestSupply();
     }
     // Just start up our machine and print some informations.
-    static void startMachine() {    
+    static void startMachine() {
+
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
@@ -23,23 +26,59 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
+
     }
-    
-    /***************************************************************** 
-    *  On the second stage, we need to inform the quantity of cups we 
-    *  want, then the program will calculate the amount of ingredients
-    *  needed to make coffee.
-    ******************************************************************/
+
+    /*****************************************************************
+     *  On the second stage, we need to inform the quantity of cups we
+     *  want, then the program will calculate the amount of ingredients
+     *  needed to make coffee.
+     ******************************************************************/
     static void howManyCoffee() {
+
         System.out.println("Write how many cups of coffee you will need: ");
         int amountOfCoffe = scanner.nextInt();
         int amountOfWater = amountOfCoffe * 200;    // 200ml of water = 1 cup
         int amountOfMilk = amountOfCoffe * 50;      // 50 ml of milk = 1 cup
         int amountOfBeans = amountOfCoffe * 15;     // 15 g of coffee beans = 1 cup
-        
+
         System.out.println("For " + amountOfCoffe + " cups you will need:");
         System.out.println(amountOfWater + " ml of water");
         System.out.println(amountOfMilk + " ml of milk");
         System.out.println(amountOfBeans + " g of coffee beans");
+
+    }
+    /*****************************************************************
+    * On the third stage, we need to inform the quantity of cups we want,
+    * then the program will calculate how much coffee it can makes
+    * and show us.
+    *****************************************************************/
+    static void requestSupply() {
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterSupply = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkSupply = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beanSupply = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cupQuantity = scanner.nextInt();
+
+        waterSupply = waterSupply / 200;
+        milkSupply = milkSupply / 50;
+        beanSupply = beanSupply / 15;
+
+        int minimum = Math.min(waterSupply, milkSupply); // To check the minimum amount of coffee that can be made.
+        minimum = Math.min(beanSupply, minimum);         // To check the minimum amount of coffee that can be made.
+
+        if (cupQuantity == minimum) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupQuantity < minimum) {
+            System.out.println("Yes, I can make that amount of coffee (and even " +
+                    (minimum - cupQuantity) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + minimum + " cup(s) of coffee");
+        }
+
     }
 }
