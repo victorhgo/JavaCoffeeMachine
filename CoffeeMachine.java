2984e1bb9d950b6904f64f9e504928748fc3a549
Victor Hugo <39852859+victorhgo@users.noreply.github.com>
2020-06-20 17:26:38 -0300

    On a Coffee Loop
    Add the do, while to create a loop

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,3 +1,4 @@
+ 
 /********************************************************
  * Created by Victor Hugo on 16th June, 2020.
  * Learning Java with JetBrains Academy Java Course.
@@ -11,74 +12,128 @@ import java.lang.Math;
 public class CoffeeMachine {
     final static Scanner scanner = new Scanner(System.in);
 
+    static int water = 400;
+    static int milk = 540;
+    static int beans = 120;
+    static int cups = 9;
+    static int money = 550;
+
     public static void main(String[] args) {
-        //startMachine();
-        //howManyCoffee();
-        requestSupply();
+
+        boolean shutdown = true;
+
+        do {
+            System.out.println("Write action (buy, " +
+                    "fill, take, remaining, exit): ");
+            String option = scanner.next();
+
+            switch (option) {
+                case "buy":
+                    System.out.println("What do you want to buy?" +
+                            " 1 - espresso, 2 - latte, 3 - cappuccino, " +
+                            "back - to main menu: ");
+
+                    String buyOption = scanner.next();
+                    switch (buyOption) {
+                        case "1":
+                            buyEspresso();
+                            break;
+                        case "2":
+                            buyLatte();
+                            break;
+                        case "3":
+                            buyCappuccino();
+                            break;
+                        case "back":
+                            break;
+                    }
+                    break;
+                case "fill":
+                    System.out.println("Write how many ml of water do you want to add:");
+                    water += scanner.nextInt();
+                    System.out.println("Write how many ml of milk do you want to add:");
+                    milk += scanner.nextInt();
+                    System.out.println("Write how many grams of coffee beans do you want to add:");
+                    beans += scanner.nextInt();
+                    System.out.println("Write how many disposable cups of coffee do you want to add:");
+                    cups += scanner.nextInt();
+
+                    remaining(water, milk, beans, cups, money);
+                    break;
+                case "take":
+                    System.out.println("I gave you $" + money);
+                    money = 0;
+                    break;
+                case "remaining":
+                    remaining(water, milk, beans, cups, money);
+                    break;
+                case "exit":
+                    shutdown = false;
+                    break;
+            }
+        } while (shutdown);
+
     }
-    // Just start up our machine and print some informations.
-    static void startMachine() {
 
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-
-    }
-
-    /*****************************************************************
-     *  On the second stage, we need to inform the quantity of cups we
-     *  want, then the program will calculate the amount of ingredients
-     *  needed to make coffee.
-     ******************************************************************/
-    static void howManyCoffee() {
-
-        System.out.println("Write how many cups of coffee you will need: ");
-        int amountOfCoffe = scanner.nextInt();
-        int amountOfWater = amountOfCoffe * 200;    // 200ml of water = 1 cup
-        int amountOfMilk = amountOfCoffe * 50;      // 50 ml of milk = 1 cup
-        int amountOfBeans = amountOfCoffe * 15;     // 15 g of coffee beans = 1 cup
-
-        System.out.println("For " + amountOfCoffe + " cups you will need:");
-        System.out.println(amountOfWater + " ml of water");
-        System.out.println(amountOfMilk + " ml of milk");
-        System.out.println(amountOfBeans + " g of coffee beans");
-
-    }
-    /*****************************************************************
-    * On the third stage, we need to inform the quantity of cups we want,
-    * then the program will calculate how much coffee it can makes
-    * and show us.
-    *****************************************************************/
-    static void requestSupply() {
-
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int waterSupply = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int milkSupply = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int beanSupply = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cupQuantity = scanner.nextInt();
-
-        waterSupply = waterSupply / 200;
-        milkSupply = milkSupply / 50;
-        beanSupply = beanSupply / 15;
-
-        int minimum = Math.min(waterSupply, milkSupply); // To check the minimum amount of coffee that can be made.
-        minimum = Math.min(beanSupply, minimum);         // To check the minimum amount of coffee that can be made.
-
-        if (cupQuantity == minimum) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cupQuantity < minimum) {
-            System.out.println("Yes, I can make that amount of coffee (and even " +
-                    (minimum - cupQuantity) + " more than that)");
+    public static void buyEspresso() {
+        if (water < 250) {
+            System.out.println("Sorry, not enough water!");
+        } else if (beans < 16) {
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (cups < 1) {
+            System.out.println("Sorry, not enough cups!");
         } else {
-            System.out.println("No, I can make only " + minimum + " cup(s) of coffee");
+            System.out.println("I have enough resources, making you a coffee!");
+
+            water -= 250;
+            beans -= 16;
+            cups--;
+            money += 4;
+        }
+    }
+    public static void buyLatte() {
+        if (water < 350) {
+            System.out.println("Sorry, not enough water!");
+        } else if (beans < 20) {
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (cups < 1) {
+            System.out.println("Sorry, not enough cups!");
+        } else if (milk < 75) {
+            System.out.println("Sorry, not enough milk!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            water -= 350;
+            milk -= 75;
+            beans -= 20;
+            cups--;
+            money += 7;
+        }
+    }
+    public static void buyCappuccino() {
+        if (water < 200) {
+            System.out.println("Sorry, not enough water!");
+        } else if (beans < 12) {
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (cups < 1) {
+            System.out.println("Sorry, not enough cups!");
+        } else if (milk < 100) {
+            System.out.println("Sorry, not enough milk!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            water -= 200;
+            milk -= 100;
+            beans -= 12;
+            cups--;
+            money += 6;
         }
 
+    }
+    private static void remaining(int water, int milk, int beans, int cups, int money) {
+        System.out.println("The coffee machine has:\n" +
+                water + " of water\n" +
+                milk + " of milk\n" +
+                beans + " of coffee beans\n" +
+                cups + " of disposable cups\n" +
+                money + " of money\n");
     }
 }
